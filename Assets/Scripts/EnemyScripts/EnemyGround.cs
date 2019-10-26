ff4838180ca09ebe48b9a4cabfbe462a318d9b11
Epicrider <omarepic1@gmail.com>
2019-10-26 16:48:30 -0700

    Add Tracking and Stunned State

--- Assets/Scripts/EnemyScripts/EnemyGround.cs
+++ Assets/Scripts/EnemyScripts/EnemyGround.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class EnemyGround : Enemy
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        movement();
+        if(!stunned)
+            movement();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.Equals(target))
+            damaged();
     }
 
     protected override void movement()
     {
-        GetComponent<Rigidbody2D>().MovePosition(Vector3.MoveTowards(transform.position, 
-        tracking(target), // Target
-        speed)); // Speed
+        rb2d.MovePosition(Vector3.MoveTowards(transform.position, 
+            tracking(target), 
+            speed));
     }
 
     protected override void damaged()
     {
-        int x = 2;
+        stunned = true;
+        Debug.Log("What the fuck");
+        rb2d.AddForce( -(transform.position.normalized - tracking(target).normalized) * pushBack );
     }
 
     
