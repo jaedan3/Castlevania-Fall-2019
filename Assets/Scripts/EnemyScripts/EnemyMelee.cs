4f421813c3cb7402c4c284552c2089dd500c52b0
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-10-26 23:13:20 -0700

    updated the new sprites for anteater and melee ant. Also, changed the enemy melee  script a bit

--- Assets/Scripts/EnemyScripts/EnemyMelee.cs
+++ Assets/Scripts/EnemyScripts/EnemyMelee.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyMelee : MonoBehaviour
 {
+    public int HP = 100;
+    Rigidbody2D rg;
+    public float Xforce = 10, Yforce;
+    void movement()
+    {
+        rg.velocity = new Vector2(Xforce, rg.velocity.y);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rg = GetComponent<Rigidbody2D>();
+        rg.freezeRotation = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+            Xforce = -Xforce;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    
     }
+    
 }
