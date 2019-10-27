470da8e731510502ebcb03b8a8878781834b4abb
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-10-27 01:36:35 -0700

    fixed up the enemy melee

--- Assets/Scripts/EnemyScripts/AttackMelee.cs
+++ Assets/Scripts/EnemyScripts/AttackMelee.cs
@@ -6,29 +6,70 @@ public class AttackMelee : MonoBehaviour
 {
     public int HP = 100;
     Rigidbody2D rg;
-    public float Xforce = 10, Yforce;
-    void movement()
-    {
-        rg.velocity = new Vector2(Xforce, rg.velocity.y);
-    }
-    // Start is called before the first frame update
+    Animator animator;
+    SpriteRenderer render;
+    public float xComp = 3;
+    bool knocked = false;
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
         rg.freezeRotation = true;
+        animator = GetComponent<Animator>();
+        render = GetComponent<SpriteRenderer>();
+        render.flipX = xComp > 0 ? true : false;
     }
+    void KB(float dir)
+    {
+        rg.velocity = new Vector2(dir * 3, 7);
+        xComp = -(dir * 3);
+    }
+
+    void movement()
+    {
+        rg.velocity = new Vector2(xComp, rg.velocity.y);
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
-            Xforce = -Xforce;
-
+        if (collision.collider.tag == "Wall")//// BOUND OFF THE WALL
+        {
+            render.flipX = !render.flipX;
+            xComp = -xComp;
+        }
+        else if (collision.collider.tag == "Attack")
+        {
+            if (HP > 0)
+            {
+                KB(collision.collider.transform.position.x < transform.position.x ? 1 : -1);
+                knocked = true;
+                animator.SetBool("Air", true);
+                HP -= 20;
+            }
+            else    animator.SetTrigger("death");
+        }
     }
+
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
-        movement();
+        if (!knocked)
+        {
+            movement();
+        }
+        else if(rg.velocity.y == 0)
+        {
+            knocked = false;
+            animator.SetBool("Air", false);
+            render.flipX = xComp > 0 ? true : false;
+        }
     
     }
+    
     
 }
