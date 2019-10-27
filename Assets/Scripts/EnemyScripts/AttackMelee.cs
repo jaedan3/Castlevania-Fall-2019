bad0973be662a1748f0eace2f022b3d229bbba72
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-10-27 02:30:53 -0700

    changed the enemy melee's wall detection to raycasting

--- Assets/Scripts/EnemyScripts/AttackMelee.cs
+++ Assets/Scripts/EnemyScripts/AttackMelee.cs
@@ -8,20 +8,28 @@ public class AttackMelee : MonoBehaviour
     Rigidbody2D rg;
     Animator animator;
     SpriteRenderer render;
+    public Transform wallDetect;
     public float xComp = 3;
     bool knocked = false;
     void Start()
     {
+        /////////////////////////////// GET COMPONENTS!
         rg = GetComponent<Rigidbody2D>();
         rg.freezeRotation = true;
         animator = GetComponent<Animator>();
         render = GetComponent<SpriteRenderer>();
-        render.flipX = xComp > 0 ? true : false;
+        ///////////////////////////////
+
+
+        render.flipX = xComp > 0 ? true : false; //Sets the right direction
+        //below will set the position of the raycaster according to the direction the ant is headed
+        wallDetect.position = new Vector3(xComp > 0 ? transform.position.x + 0.75f : transform.position.x - 0.75f, wallDetect.position.y, wallDetect.position.z);
     }
-    void KB(float dir)
+
+    void KB(float dir)// Knock back
     {
-        rg.velocity = new Vector2(dir * 3, 7);
-        xComp = -(dir * 3);
+        rg.velocity = new Vector2(dir * 3, 7);// Knocked back for 3 units back, 7 units up
+        xComp = -(dir * 3); // moves towards the source of the damage
     }
 
     void movement()
@@ -32,19 +40,13 @@ public class AttackMelee : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Wall")//// BOUND OFF THE WALL
+         if (collision.collider.tag == "Attack") // When collided with an attack
         {
-            render.flipX = !render.flipX;
-            xComp = -xComp;
-        }
-        else if (collision.collider.tag == "Attack")
-        {
-            if (HP > 0)
+            if ((HP -= 20) > 0) // if HP is greator than 0, call KB(knockback)
             {
                 KB(collision.collider.transform.position.x < transform.position.x ? 1 : -1);
                 knocked = true;
                 animator.SetBool("Air", true);
-                HP -= 20;
             }
             else    animator.SetTrigger("death");
         }
@@ -58,11 +60,18 @@ public class AttackMelee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D HitWall = Physics2D.Raycast(wallDetect.position,Vector2.down, 0.01f);
+        if(HitWall.collider == true)
+        {   
+            xComp = -xComp;
+            render.flipX = !render.flipX;
+            wallDetect.position = new Vector3(xComp > 0 ? transform.position.x + 0.75f : transform.position.x - 0.75f, wallDetect.position.y, wallDetect.position.z);
+        }
         if (!knocked)
         {
             movement();
         }
-        else if(rg.velocity.y == 0)
+        else if (rg.velocity.y == 0)
         {
             knocked = false;
             animator.SetBool("Air", false);
