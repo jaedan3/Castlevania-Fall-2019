e59ea40a15fafbca908f794f5680266b4524a1e3
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-10-30 16:40:37 -0700

    changed scene

---
+++ Assets/Player/Animation/New Animation/SpiritScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritScript : MonoBehaviour
+{
+    Color alp;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        alp = GetComponent<SpriteRenderer>().color;
+        alp.a = 255;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime);
+
+        alp.a -= 5;
+       // GetComponent<SpriteRenderer>().color = alp;
+    }
+}
