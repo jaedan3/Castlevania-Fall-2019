57393a5a4cfaab7b18fdac4755fbeb7c992cecbd
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-11-10 18:38:36 -0800

    Added sinusoidal movement of floating enemy

---
+++ Assets/sinussoidal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sinussoidal : MonoBehaviour
+{
+    private Vector2 Position;
+    public float amp = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(Position.x,Position.y + amp * Mathf.Sin(Time.time * 2));
+    }
+}
