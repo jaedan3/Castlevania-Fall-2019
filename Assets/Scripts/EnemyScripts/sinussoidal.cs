6f16832bee09f1d84694ba2385c6189a1102c896
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-11-11 20:02:39 -0800

    added the correct movement to bees and ants You need to assign the player to be assigned on the flyinh enemy script

--- Assets/Scripts/EnemyScripts/sinussoidal.cs
+++ Assets/Scripts/EnemyScripts/sinussoidal.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class sinussoidal : MonoBehaviour
 {
-
-    private Vector2 Position;
+    
+    private Vector3 Position;
+    private Vector3 Velo = new Vector2(0, 0);
+    private bool dying;
+    public float MaxSpeed;
     public float amp = 2;
+    public float accel = 1;
     public float floating_rate = 2;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +21,27 @@ public class sinussoidal : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
         transform.position = new Vector2(Position.x,Position.y + amp * Mathf.Sin(Time.time * floating_rate));
+        setMovement();
+        Position += Velo;
+        
     }
+    void setMovement()
+    {
+        Vector3 accelVector = (player.transform.position - Position).normalized * accel;
+        Debug.Log(accelVector);
+        Velo += accelVector;
+        Debug.Log(accelVector + "<- accel and velo->" +Velo);
+        transform.rotation = Quaternion.Euler(0, accelVector.x < 0 ? 0:180, 0);
+        if (MaxSpeed < Velo.magnitude)
+        {
+            Velo *= (MaxSpeed / Velo.magnitude);
+        }
+    
+    }
+    
+
 }
