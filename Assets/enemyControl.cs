5b3cc2761ba826b53a588f75c291f0d80aef0f68
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-11-12 13:08:26 -0800

    enemy spawn

---
+++ Assets/enemyControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyControl : MonoBehaviour
+{
+    public GameObject ToBeSpawned;
+    public GameObject Player;
+    public Vector2 spawnLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "player")
+        {
+            SpawnEnemy(ToBeSpawned, spawnLocation);
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy(GameObject Type, Vector3 loc)
+    {
+        GameObject spawned = Instantiate(Type, loc, Quaternion.identity);
+        spawned.GetComponent<Enemy>().target = Player;
+    }
+}
