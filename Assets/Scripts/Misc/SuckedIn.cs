ee09bbdcba746bc3bd0d5a93fdaabc756913af3f
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-11-12 19:03:43 -0800

    added the health display(temp)

--- Assets/Scripts/Misc/SuckedIn.cs
+++ Assets/Scripts/Misc/SuckedIn.cs
@@ -7,6 +7,7 @@ public class SuckedIn : MonoBehaviour
     public GameObject player;
     public float speed;
     public int healAmount;
+    private bool expired = false;
     // Start is called before the first frame update
 
     private void Awake()
@@ -21,8 +22,12 @@ public class SuckedIn : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
         if ((transform.position - player.transform.position).magnitude < 0.1) 
         {
-            player.GetComponent<PlayerHurtbox>().health += healAmount;
             GetComponent<ParticleSystem>().Play(false); //STOPS EMITTING THE EXTRA BLODD
+            if (!expired)
+            {
+                player.GetComponent<PlayerHurtbox>().health += healAmount;
+                expired = true;
+            }
             Destroy(gameObject,0.5f);//DESTROYS AFTER A DELAY SO THE PARTICLES DOES NOT DISAPPEAR IMMEDIATELY
         }
     }
--- Assets/TempHealthDisplay.cs
+++ Assets/TempHealthDisplay.cs
@@ -18,6 +18,6 @@ public class TempHealthDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = "" + PHB.health;   
+        text.text = "Health: " + PHB.health;   
     }
 }
