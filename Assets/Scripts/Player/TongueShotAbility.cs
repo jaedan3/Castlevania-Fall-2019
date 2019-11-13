604c7cf49ac9b7280538cb33c510e9525c0ded91
Epicrider <omarepic1@gmail.com>
2019-11-13 13:47:17 -0800

    Fix Hookshot Glitches

--- Assets/Scripts/Player/TongueShotAbility.cs
+++ Assets/Scripts/Player/TongueShotAbility.cs
@@ -7,12 +7,19 @@ public class TongueShotAbility : MonoBehaviour
     private Vector3 tongueDirection;
     private RaycastHit2D objHit;
     private Rigidbody2D rb2d;
+    private Vector3 ogPosition;
+    private float distanceToTravel;
+    private bool hooking;
+    private float shortenDistance;
     public float reach;
     public float pullPower;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        shortenDistance = 0.3f;
     }
 
     // Update is called once per frame
@@ -21,18 +28,19 @@ public class TongueShotAbility : MonoBehaviour
 
         tongueDirection = ( new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) ).normalized;
         RaycastHit2D HitWall = Physics2D.Raycast(transform.position, tongueDirection,1 );
-        if (HitWall.collider == true)
+        if (hooking){ CheckPosition(); }
+        /*if (HitWall.collider == true)
         {
             rb2d.velocity = Vector2.zero;
-        }
-        if (Input.GetKeyDown(KeyCode.L))
+        }*/
+        else if (Input.GetKeyDown(KeyCode.L))
         {
             Debug.DrawLine(transform.position, transform.position + ( tongueDirection * reach ) );
             objHit = Physics2D.Raycast(transform.position, tongueDirection, reach);
             if ( objHit.collider != null )
             {
                 Debug.Log("Hit");
-                if (objHit.collider.tag == "Wall") // collider is not the collider component, it is the object that was collided with
+                if (objHit.collider.tag == "Wall" && !hooking) // collider is not the collider component, it is the object that was collided with
                 {
                     Debug.Log("Hooked");
                     pullTo(tongueDirection);
@@ -42,6 +50,18 @@ public class TongueShotAbility : MonoBehaviour
     }
 
     void pullTo(Vector3 destination){
+        hooking = true;
         rb2d.velocity = destination * pullPower;
+        ogPosition = transform.position;
+        distanceToTravel = objHit.distance-shortenDistance;
     }
+
+    void CheckPosition(){
+        if ( Vector3.Distance(ogPosition, transform.position) >= distanceToTravel )
+        {
+            rb2d.velocity = new Vector2(0f,0f);
+            hooking = false;
+        }
+    }
+
 }
