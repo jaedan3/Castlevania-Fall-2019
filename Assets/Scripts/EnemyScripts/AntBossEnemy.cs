c131fd8669cb3f6d6a17ac966bf7f9349b713f3e
Epicrider <omarepic1@gmail.com>
2019-11-23 16:26:08 -0800

    Add Ant Boss Enemy Script

---
+++ Assets/Scripts/EnemyScripts/AntBossEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntBossEnemy : Enemy
+{
+
+    private Animator anim;
+    private int eatAntHash = Animator.StringToHash("");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    protected override void movement()
+    {
+        return;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Hit");
+        if ( col.gameObject.GetComponent<EnemyGround>() != null )
+        {
+            anim.SetTrigger(eatAntHash);
+        }
+    }
+
+}
