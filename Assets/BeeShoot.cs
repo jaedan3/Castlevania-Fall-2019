f0c234dd4c67043ea5e1d8cb84761ebfdbfedfb2
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2019-11-27 13:38:10 -0800

    added some movements for queen bee, and added a prefab for qheen's projectile

---
+++ Assets/BeeShoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeShoot : Enemy
+{
+    public Vector3 dir;
+    Animator animator;
+    public void init(Vector2 direction)
+    {
+        dir = direction;
+        animator = GetComponent<Animator>();
+        GetComponent<SpriteRenderer>().flipX = dir.x > 0 ? true : false;
+        Destroy(gameObject, 5);
+        dying = false;
+    }
+    // Update is called once per frame
+    override protected void movement()
+    {
+
+    }
+    void Update()
+    {
+        transform.position += dir;   
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Attack" && !dying) // When collided with an attack
+        {
+            animator.SetTrigger("death");
+            dir = Vector2.zero;/////########### DROPS when dead
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            GetComponent<BoxCollider2D>().isTrigger = false;
+            Destroy(gameObject, 0.5f);
+            dying = true;
+        }
+    }
+}
---
+++ Assets/QueenBee.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QueenBee : Enemy
+{
+    private Vector3 direction;
+    public float AirDrag;
+    public int Delay;
+    public bool ready;
+    private int AttackCount = 3;
+    private float Speed;
+    bool DASH = false;
+    public GameObject projectile;
+    private int phase = 0;
+    public float projSpeed = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void movement()
+    {
+        transform.position = transform.position + direction * Speed;
+    }
+
+    Vector2 getDir()
+    {
+        return (target.transform.position - transform.position).normalized;
+    }
+    void dash()
+    {
+        Speed -= AirDrag;
+        if(Speed < 0)
+        {
+            direction = getDir();
+            if (AttackCount > 0)
+            {
+                AttackCount--;
+                Speed = 0.8f;
+                Debug.Log(AttackCount);
+            }
+            else
+            {
+                DASH = false;
+                phase = 1;
+            }
+        }
+
+    }
+    void shoot()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+
+            GameObject a = Instantiate(projectile, transform.position, Quaternion.identity);
+            a.GetComponent<BeeShoot>().init(getDir() * projSpeed + new Vector2(0.02f * (i*i),0));
+
+        }
+        phase = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        movement();
+        GetComponent<SpriteRenderer>().flipX = getDir().x > 0 ? true : false;
+        if (Delay == 0 && ready)
+        {
+            if (phase == 1)
+            {
+                shoot();
+                Delay = 100;
+            }
+            else
+            {
+                DASH = true;
+                AttackCount = 3;
+                Delay = 250;
+            }
+        }
+        else//############################### Delay after attack
+        {
+            Delay--;
+            if (DASH)
+            {
+                dash();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
